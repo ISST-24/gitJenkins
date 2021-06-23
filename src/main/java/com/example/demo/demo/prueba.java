d278baf832b5861912b0b13f4ed21e3501214bff
Jasle Carrasco Galiano <jasle.carrasco@babel.es>
2021-06-23 13:58:49 +0200

    java añadido

---
+++ src/main/java/com/example/demo/demo/prueba.java
@@ -0,0 +1,9 @@
+package com.example.demo.demo;
+
+public class prueba {
+
+    public static void main(String[] args) {
+        Integer num = null;
+        System.out.println("hola mundo"+ num);
+    }
+}
